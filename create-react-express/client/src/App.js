af77a7bcebda9968ee672708e67dfa34700a0a11
Casey Moldavon <cmoldavon@gmail.com>
2020-02-12 11:56:37 -0800

    Adding Media content

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar/index"
+import Navbar from "./components/Navbar/index";
 import "./App.css";
 import Jumbotron from "./components/Jumbotron/index";
 import Search from "./components/Search/index";
 import { Container } from "react-bootstrap";
+import Media from "./components/Media/index";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
     <Search/>
 
     <Container className="results">
-      
+      <h3>Results</h3>
+      <Media/>
     </Container>
     </div>
   );
