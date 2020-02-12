c5c773b182f44c30a775b1fc230d5e5aa2036c70
Casey Moldavon <cmoldavon@gmail.com>
2020-02-12 11:33:50 -0800

    Updating components

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar/index"
-import Search from "./components/Search/index"
 import "./App.css";
+import Jumbotron from "./components/Jumbotron/index";
+import Search from "./components/Search/index";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div>
     <Navbar/>
+    <Jumbotron/>
+
     <Search/>
+
+    <Container className="results">
+      
+    </Container>
     </div>
   );
 }
