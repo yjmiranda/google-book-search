d3cba5495ced487f90dca24740cc7abd96734745
Yali Miranda <yalimiranda@gmail.com>
2020-02-12 11:28:22 -0800

    created book model, and added api routes to server'

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,12 +14,46 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.get("/api/books",(req,res)=>{
+  db.Books.find({})
+    .then(dbBooks =>{
+      res.json(dbBooks);
+    }).catch(err =>{
+      res.send(err);
+    });
+});
+
+app.post("/api/books", (req,res)=>{
+  db.Books.create({
+    title: req.body.title,
+    authors: req.body.authors,
+    description: req.body.description,
+    image: req.body.image,
+    link: req.body.link
+  }).then(dbBooks =>{
+    res.json(dbBooks);
+  }).catch(err =>{
+    res.send(err);
+  });
+});
+
+app.delete("/api/books/:id", (req,res)=>{
+  db.Books.deleteOne({_id: req.params.id})
+    .then(book=>{
+      res.json(book);
+    }).catch(err=>{
+      res.send(err);
+    });
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
