5c8e088e53c164e7b56e1a4ea907f940a881a09e
Christopher Melby <melby.christopher@gmail.com>
2020-02-15 16:03:20 -0800

    adding saved books.js

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,20 +1,32 @@
-import React,{useState} from "react";
+import React from "react";
 import Navbar from "./components/Navbar/index";
 import "./App.css";
-import LookFor from "./pages/LookFor"
-import { Container } from "react-bootstrap";
-import Media from "./components/Media/index";
-//import API from "../utils/API"
+// import Jumbotron from "./components/Jumbotron/index";
+// import SearchBar from "./components/Search/index";
+// import { Container } from "react-bootstrap";
+// import Media from "./components/Media/index";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved"
+
 
 
 function App() {
   return (
     <div>
-
-    <LookFor  />
+      <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path="/" component={search} /> */}
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
+    </Router>
+      
     </div>
   );
 }
 
 
 export default App;
+
--- create-react-express/client/src/pages/Saved.js
+++ create-react-express/client/src/pages/Saved.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import { Container, List, Button } from 'react-bootstrap';
+import { Container, Button, Card, Col, Row, Jumbotron } from 'react-bootstrap';
 import API from '../utils/API';
+import Header from "../components/Jumbotron/index";
+import "./LookFor.css"
+
 
 
 class Saved extends Component {
     state = {
-        books =[],
-        title = "",
-        authors = "",
-        description ="",
-        image = "",
-        link = ""
+        books: [],
+        title: "",
+        authors: "",
+        description: "",
+        image:"",
+        link: ""
     };
 
     componentDidMount() {
+        console.log("component did mount")
         this.loadBooks()
     };
 
     loadBooks = () => {
+        console.log("insode load books")
         API.getBooks()
-            .then(res =>
-                this.setState({ books: res.data, title ="", authors: "", description: "", image: "", link: "" })
-                    .catch(err => console.log(err))
-            )
+        
+            .then(res => {
+                console.log(res.data)
+                this.setState({ books: res.data, title: res.data.title, authors: res.data.authors, description: res.data.description, image: res.data.image, link: res.data.link })
+                })
+                .catch(err => console.log(err))
+
     }
 
     deleteBook = id => {
@@ -34,36 +42,48 @@ class Saved extends Component {
 
     render() {
         return (
-            <Container>
+            <div>
+            <Header/>
+            <Container classNamme="results">
+                <h1 id="result">Your Saved Books:</h1>
+                 <Row className="justify-content-center">
                 {this.state.books.length ? (
-                    <List>
-                        {this.state.books.map(book => (
-                            <ListItem key={book._id}>
-                                <img src={book.image} />
-                                <strong>
-                                    {book.title} by {book.authors}
-                                </strong>
-                                <p>{book.description}</p>
-                                <Button href={book.link}>View</Button>
-                                <Button onClick={() => this.deleteBook(book._id)}>Delete</Button>
-                            </ListItem>
-                        ))}
-                    </List>
+                    <Row>
+                        <Col>
+                            {this.state.books.map(book => (
+                            <Card key={book.title} style={{ width: '100%', margin: '10px'}} className="shadow-sm">
+                                <Row>
+                                <Col className="col-3">
+                                    <Card.Img className="ml-5 pl-5 pt-5" style={{width: "45%"}} src={book.image}></Card.Img>
+                                </Col>
+                                <Col className="col-9">
+                                <Card.Body>
+                                <Button variant="danger" className="float-right" onClick={() => this.deleteBook(book._id)}>Delete</Button>
+                                <Button className="float-right mr-2" href={book.link}>View</Button>
+                                    <Card.Text ><strong>{book.title}</strong></Card.Text>
+                                    <Card.Text >Written By: <strong>{book.authors}</strong></Card.Text>
+                                    <Card.Text >{book.description}</Card.Text>
+                                </Card.Body>
+                                </Col>
+                                </Row>
+                                </Card>
+                            ))}
+                        </Col>
+                    </Row>
                 ) : (
-                        <h3>No Results to Display</h3>
-                    )}
+                    <h3>No Results to Display</h3>
+                )}
+             </Row>  
+   
             </Container>
+            </div>
         )
     };
-
-
-
-
-
-
-
 
 
 }
 
 export default Saved;
+
+
+
