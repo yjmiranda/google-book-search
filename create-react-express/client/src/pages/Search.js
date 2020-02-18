41ec558a919924bc7df4bed46b014e51e8ce5fc4
Casey Moldavon <cmoldavon@gmail.com>
2020-02-18 15:16:06 -0800

    Removing Search component folder

--- create-react-express/client/src/pages/Search.js
+++ create-react-express/client/src/pages/Search.js
@@ -1,25 +1,47 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron/index";
-import Search from "../components/Search/index";
-import { Container, Media, Button, Image } from "react-bootstrap";
+// import Search from "../components/Search/index";
+import { Container, Media, Button, Image, Form } from "react-bootstrap";
+
 import API from "../utils/API";
 
-// import "./style.css";
+import "./style.css";
 
 
 class Find extends Component {
 
     state = {
         books: [],
-        title: "Old Man and The Sea",
+        title: "",
         authors: "",
         description:"",
         image: "",
-        link: ""
+        link: "",
     }
 
     componentDidMount(){
         this.LoadBook()
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value 
+        });
+        // console.log(event.target);
+        // console.log(this.state.title);
+    }
+
+    handleFormSubmit = event => {
+        event.preventDafault();
+
+        // console.log(this.state.title);
+
+        API.searchBooks(this.state.title)
+        .then(res => this.setState({
+            books: res.data.items
+        }))
+        .catch( err => console.log(err));
     }
 
 
@@ -27,6 +49,7 @@ class Find extends Component {
         API.searchBooks(this.state.title)
         .then(res => {
             this.setState({books: res.data.items})
+
 
             console.log(res.data.items);
             // console.log(this.state.books);
@@ -39,11 +62,33 @@ class Find extends Component {
             <div>
             <Jumbotron/>
 
-            <Search/>
+
+            <Container className="search-container">
+                <h4>Book Search</h4>
+
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Book</Form.Label>
+
+                        <Form.Control  name="title" value={this.state.title} onChange={this.handleInputChange} type="text" placeholder="Search" />
+
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" onClick={this.handleFormSubmit}>
+                        Submit
+                    </Button>
+
+                </Form>
+            </Container>
+
+
+
+
+
+
 
             <Container className="results">
             <h3 id="result">Results:</h3>
-
 
 
                 {this.state.books.map(book => (
@@ -52,11 +97,11 @@ class Find extends Component {
                         <Media.Body>
 
                             <Image
-                            width={100}
-                            height={100}
-                            className="align-self-start mr-3"
-                            src={book.volumeInfo.imageLinks.thumbnail}
-                            alt="Image"
+                                width={100}
+                                height={100}
+                                className="align-self-start mr-3"
+                                src={book.volumeInfo.imageLinks.thumbnail}
+                                alt="Image"
                             />
 
                             <h5><strong>
@@ -68,9 +113,8 @@ class Find extends Component {
                             </strong></h6>
 
                             <p>
-                                Description: {book.volumeInfo.description}
+                                <strong>Description:</strong> {book.volumeInfo.description}
                             </p>
-                            {/* <p src="{book.volumeInfo.accessInfo.webReaderLink}">{book.volumeInfo.title}</p> */}
 
                             </Media.Body>
                         <Button variant="danger">View</Button>
