39850c907efe9ce49471e33795147c3fbefd3ab6
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-02-18 16:00:20 -0800

    the search works now

--- create-react-express/client/src/pages/Search.js
+++ create-react-express/client/src/pages/Search.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron/index";
-import Search from "../components/Search/index";
-import { Container, Media, Button, Image } from "react-bootstrap";
+import { Container, Media, Button, Image,Form,FormControl } from "react-bootstrap";
 import API from "../utils/API";
-
-// import "./style.css";
+import "./style.css";
 
 
 class Find extends Component {
 
     state = {
         books: [],
-        title: "Old Man and The Sea",
+        title: "",
         authors: "",
         description:"",
         image: "",
@@ -21,11 +19,25 @@ class Find extends Component {
     componentDidMount(){
         this.LoadBook()
     }
+    handleInputChange = event =>{
+        const {name, value } = event.target;
+        this.setState({
+            [name] : value 
+        })
+    }   
 
+    handleFormSubmit = event =>{
+        event.preventDefault()
+        API.searchBooks(this.state.title)
+        .then(res => this.setState({books:res.data.items}))
+                  
+        .catch(err => console.log(err))
+    }
 
     LoadBook = () => {
         API.searchBooks(this.state.title)
         .then(res => {
+        
             this.setState({books: res.data.items})
 
             console.log(res.data.items);
@@ -38,34 +50,63 @@ class Find extends Component {
         return (
             <div>
             <Jumbotron/>
+        
+            <Container className="search-container">
+                <h4>Book Search</h4>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Book</Form.Label>
+                        <Form.Control  name="title" value={this.state.title} onChange={this.handleInputChange} type="text" placeholder="Search" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={this.handleFormSubmit}>
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
 
-            <Search/>
 
             <Container className="results">
             <h3 id="result">Results:</h3>
 
-
-
+            <div>
                 {this.state.books.map(book => (
+
                 <div className="media-div">
                     <Media key={book.title}>
                         <Media.Body>
-
-                            <Image
-                            width={100}
-                            height={100}
-                            className="align-self-start mr-3"
-                            src={book.volumeInfo.imageLinks.thumbnail}
-                            alt="Image"
-                            />
-
+                            {book.volumeInfo.imageLinks ? (
+                                <Image
+                                width={100}
+                                height={100}
+                                className="align-self-start mr-3"
+                                src={book.volumeInfo.imageLinks.thumbnail} 
+                                alt="Image"
+                                />
+                            ) : (
+                                <Image
+                                width={100}
+                                height={100}
+                                className="align-self-start mr-3"
+                                src="placeholder" 
+                                alt="Image"
+                                />
+                            )
+                            }
                             <h5><strong>
                                 title: {book.volumeInfo.title}
                             </strong></h5>
-
-                            <h6><strong>
+                            
+                            {book.volumeInfo.authors? (
+                                <h6><strong>
                                 Author: {book.volumeInfo.authors[0]}
                             </strong></h6>
+                            ):(
+                                <h6><strong>
+                                Author: Not Defined
+                            </strong></h6>
+                            )
+                            }
+
 
                             <p>
                                 Description: {book.volumeInfo.description}
@@ -77,8 +118,10 @@ class Find extends Component {
                         <Button variant="success">Save</Button>
                     </Media>
                 </div>
-                ))}
-
+                
+                ))} 
+                </div>
+            
 
             </Container>
             </div>
